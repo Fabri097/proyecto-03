0937dfd1e0f556e21cea73ca2c350f2f24332f74
Fabricio <fabrimarce2004@gmail.com>
2026-06-07 09:26:24 -0500

    fix: instalar dependencias faltantes

--- src/store/financeStore.ts
+++ src/store/financeStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware'; // Importamos el middleware
+import { persist } from 'zustand/middleware';
 
 export interface Transaction {
   id: string;
